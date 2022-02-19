30111303416a2c0f4eca435b0c8e27ac04452179
kiddy-11 <98145022+kiddy-11@users.noreply.github.com>
2022-02-19 10:29:26 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,77 @@
+
+
+// [ HW 1 MATHMETICAL OPERATION(+,-,*,/,%,**)]
+
+// (+)
+var a = 3000;
+document.write( a += 2000)
+
+// (-)
+var b = 2000;
+document.write( b -= 5000)
+
+// (*)
+var c =100 ;
+document.write( c *= 500)
+
+// (/)
+var d = 400;
+document.write( d /= 100)
+
+// (%)
+var e = 500;
+document.write( e %=100)
+
+// (**)
+var f = 1000;
+document.write( f **= 30)
+
+// Comparison operator
+
+var a = 24;
+var b = 25;
+
+document.write( a==b)
+
+var a = 11;
+var n = 11;
+document.write( a==n)
+
+ var n = '18';
+ var a = 18;
+ document.write( n==a)
+
+ var n = '18';
+ var a =  18;
+ document.write( n===a)
+
+ var n = 18;
+ var a = 18;
+ document.write( n!=a)
+
+ var n = '18';
+ var a = 18;
+ document.write( n!=a)
+
+ var n = '11';
+ var a = 18;
+ document.write( n>a)
+ document.write(n<a)
+
+ var n = 18;
+ var a = 18; 
+ document.write( n<=a)
+
+ var t = 500;
+ var s = 500;
+
+ var n = 1800;
+ var a = 1100;
+
+ document.write( t == s && n>a)
+
+var Name = ["Israt","Nigar","Isme","Nusrat","Sabata"];
+
+ for (var i = 0;i <= 10;i++){
+     document.write( Name + "<br>")
+ }
